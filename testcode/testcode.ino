b389ab4f35a3e085bfe2d329ebe8b03799e6dc38
Danielc3388 <102593818+Danielc3388@users.noreply.github.com>
2023-02-23 19:51:45 +0800

    source code

--- testcode/testcode.ino
+++ testcode/testcode.ino
@@ -1,27 +1,42 @@
+/*
+
+Function :  To read data from arduino board and send the data
+            collected to html websites and bluetooth
+
+Author : Daniel Chan
+Created on : Jan 2023
+Last modified : 23 Feb 2023
+Use with project : Gas sensor
+
+*/
+
+
+//library used
 #include <SPI.h>
 #include <Ethernet.h>
 #include <SoftwareSerial.h>
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-int a, b, c, d;
+int a, b, c, d; //variables for gas sensor data to store
 
 
 byte mac[] = {
-  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED
+  0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED //mac address for website
 };
-IPAddress ip(192, 168, 1, 177);
+IPAddress ip(192, 168, 1, 177); //IP address for the ethernet
+                                //Unique in local network
 
 
 EthernetServer server(80);
-SoftwareSerial BTSerial (4, 3);
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+SoftwareSerial BTSerial (4, 3);   //bluetooth pin
+LiquidCrystal_I2C lcd(0x27, 16, 2);   //LCD pin
 
 
 
 void createWeb() {
   EthernetClient client = server.available();
-  if (client) {
+  if (client) { //when accessed
     Serial.println("new client");
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -29,6 +44,7 @@ void createWeb() {
         char c = client.read();
         Serial.write(c);
 
+//writing html
         if (c == '\n' && currentLineIsBlank) {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
@@ -57,6 +73,7 @@ void createWeb() {
           client.print(0);
           client.println("</TD>");
 
+//create table data
           client.print("<TD class=\"value\">");
           client.print(a);
           if (a > 0) {
@@ -70,6 +87,9 @@ void createWeb() {
           client.println("</TR>");
           //
           client.println("</TABLE>");
+
+//end of table
+
           client.println("<p>System Created by Daniel Chan</p>");
           client.println("</main>");
           client.println("</html>");
@@ -90,7 +110,7 @@ void createWeb() {
 
 void EthDisabled() {
   EthernetClient client = server.available();
-  if (client) {
+  if (client) {   //if accessed
     Serial.println("new client");
     boolean currentLineIsBlank = true;
     while (client.connected()) {
@@ -98,6 +118,7 @@ void EthDisabled() {
         char c = client.read();
         Serial.write(c);
 
+//start creating html
         if (c == '\n' && currentLineIsBlank) {
           client.println("HTTP/1.1 200 OK");
           client.println("Content-Type: text/html");
@@ -116,6 +137,8 @@ void EthDisabled() {
           client.println("</html>");
           break;
         }
+//end of html
+
         if (c == '\n') {
           currentLineIsBlank = true;
         } else if (c != '\r') {
@@ -131,6 +154,7 @@ void EthDisabled() {
 
 void sendBT() {
 
+//sending data via BlueTooth
   BTSerial.print(a);
   BTSerial.print(" ");
   BTSerial.print(b);
@@ -145,15 +169,15 @@ bool EthernetEnable = LOW;
 bool BTEnable = LOW;
 
 void setup() {
-  lcd.init();
+  lcd.init(); //initialize LCD
   lcd.backlight();
   Serial.begin(9600);
-  BTSerial.begin(9600);
+  BTSerial.begin(9600); //initialize bluetooth
   while (!Serial) {
   }
-  Serial.println("Ethernet WebServer Example");
+  Serial.println("Ethernet");
 
-  Ethernet.begin(mac, ip);
+  Ethernet.begin(mac, ip); //initialize ethernet
 
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
@@ -176,29 +200,32 @@ void setup() {
 
 
 void loop() {
-  EthernetEnable = !digitalRead(2);
-  BTEnable = !digitalRead(5);
+  EthernetEnable = !digitalRead(2); //read if sending data with ethernet
+  BTEnable = !digitalRead(5); //read if sending data with bluetooth
+
+//reading sensor data
   a = analogRead(0);
   b = analogRead(1);
   c = analogRead(2);
   d = analogRead(3);
-  lcd.clear();
-  lcd.setCursor(0,0);
+  
+  lcd.clear();  //clear data on LCD
+  lcd.setCursor(0,0);   //set the cursor of LCD to first row, first digit
 
   if (EthernetEnable == HIGH) {
-    lcd.print("IP:192.168.1.177");
+    lcd.print("IP:192.168.1.177");  //notify the user the ip address of data
     createWeb();
   } else {
     EthDisabled();
-    lcd.print("Ethernet Disabled");
+    lcd.print("Ethernet Disabled");   //notify the user ethernet is disabled
   }
-  lcd.setCursor(0,1);
+  lcd.setCursor(0,1); //set the cursor of LCD to second row, first digit
   if (BTEnable == HIGH) {
-    lcd.print("BT Enabled");
+    lcd.print("BT Enabled");  //notify the user bluetooth is connected
     sendBT();
     
   } else{
-    lcd.print("BT Disabled");
+    lcd.print("BT Disabled"); //norifying the user bluetooth is disabled
   }
   delay(50);
 }
